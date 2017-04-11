ff615d565424d229199e34e12b043b1662032e1b
João André Martins <joaomartins@google.com>
2017-04-11 16:00:46 -0400

    SpringBoot skeleton application.

---
+++ src/main/java/com/google/gfx/GfxApplication.java
@@ -0,0 +1,12 @@
+package com.google.gfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfxApplication.class, args);
+	}
+}
