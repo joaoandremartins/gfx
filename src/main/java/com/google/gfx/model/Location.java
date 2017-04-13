01d4869a4697b68cd9ca29c31408534a6d40d961
João André Martins <joaomartins@google.com>
2017-04-13 14:43:55 -0400

    Adds emailAddresses to Location.

--- src/main/java/com/google/gfx/model/Location.java
+++ src/main/java/com/google/gfx/model/Location.java
@@ -1,14 +1,25 @@
 package com.google.gfx.model;
 
+import java.util.Collection;
+
 /**
- * Used to localise users.
+ * Used to localise users and orders.
  */
 public class Location {
 
+  /**
+   * Full name of the location.
+   */
   private String name;
 
-  public Location(String name) {
+  /**
+   * List of email addresses to email whenever there is a new order for that location.
+   */
+  private Collection<String> emailAddresses;
+
+  public Location(String name, Collection<String> emailAddresses) {
     this.name = name;
+    this.emailAddresses = emailAddresses;
   }
 
   public String getName() {
@@ -17,5 +28,13 @@ public class Location {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public Collection<String> getEmailAddresses() {
+    return emailAddresses;
+  }
+
+  public void setEmailAddresses(Collection<String> emailAddresses) {
+    this.emailAddresses = emailAddresses;
   }
 }
