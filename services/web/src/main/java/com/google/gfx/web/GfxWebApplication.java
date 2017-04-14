18601addffbd87a53a6487936284e207bc38f6de
João André Martins <joaomartins@google.com>
2017-04-14 17:14:20 -0400

    Renames to GfxWebApplication

--- services/web/src/main/java/com/google/gfx/web/GfxWebApplication.java
+++ services/web/src/main/java/com/google/gfx/web/GfxWebApplication.java
@@ -1,12 +1,12 @@
-package com.google.gfx;
+package com.google.gfx.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GfxApplication {
+public class GfxWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GfxApplication.class, args);
+		SpringApplication.run(GfxWebApplication.class, args);
 	}
 }
