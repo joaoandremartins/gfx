8a50580b95a5b916c4ae762fea483e277eb12aef
João Martins <joaoandremartins@users.noreply.github.com>
2017-04-20 10:34:36 -0400

    Data model and service interfaces (#2)
    * Data model and service interfaces
    
    * Adds constructors and removes useless id field,
    
    * Turns Location into a class.
    Makes amounts and exchange rates BigDecimals due to float rounding issues.
    OrderService.list methods return lists instead of collections, for ordering.
    
    * Adds emailAddresses to Location.
    
    * Created LocationMailingList.
    Added builders to Match and Order.
    
    * Reorganises project structure so that model is in its own module and all services are in the same module.
    
    * Sets packaging=jar in new service modules.
    
    * New lines in pom.xml.
    
    * Renames to GfxWebApplication
    
    * Renames parent pom to services and adds name.
    
    * Some more tidbit fixes.
    
    * Removes unnecessary pom stuff.
    
    * boolean -> Boolean

---
+++ services/matching/src/main/java/com/google/gfx/matching/MatchingService.java
@@ -0,0 +1,70 @@
+package com.google.gfx.matching;
+
+import com.google.gfx.Match;
+import com.google.gfx.Order;
+import com.google.gfx.User;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Matches existing orders with other existing or new ones.
+ */
+public interface MatchingService {
+
+  /**
+   * This method was conceived to be ran as part of a cron job that matches existing buy and sell
+   * orders.
+   */
+  void matchExistingBuyAndSellOrders();
+
+  /**
+   * Tries to match a created order with another existing order.
+   *
+   * <p>If the given order is a buy order, it will be matched with sell orders, and vice-versa.
+   * Querying the existing orders is left to the method.
+   *
+   * @return zero matches if no matching offers exist, one match if an order for the same amount
+   * exists or multiple matches if multiple orders sum up to the amount of the created order
+   */
+  List<Match> tryMatchOrder(Order newOrder);
+
+  /**
+   * Matches an existing buy and sell order.
+   *
+   * <p>Useful in the case where a user files, for example, a buy order, and there are existing sell
+   * orders that can fulfill it.
+   *
+   * @return the created match, which should be subject to buyer and/or seller approval
+   */
+  Match matchOrder(Order buyOrder, Order sellOrder);
+
+  /**
+   * Creates a match for an order, that must be accepted by the order owner.
+   *
+   * <p>Useful in the case where a user finds an order they are interested in and match it fully or
+   * partially, without explicitly creating a new order.
+   *
+   * @param order original posted order
+   * @param matchingUser the user who responded to the match
+   * @param amount amount of the match, which can fully or partially match the original order
+   * @return the created match, which should be subject to buyer or seller approval
+   */
+  Match matchOrder(Order order, User matchingUser, BigDecimal amount);
+
+  /**
+   * Accepts a proposed match.
+   *
+   * @param match proposed match
+   * @param deciderType BUY if the user deciding is the buyer, or SELL if they are the seller
+   */
+  void acceptMatch(Match match, Order.Type deciderType);
+
+  /**
+   * Declines a proposed match.
+   *
+   * @param match proposed match
+   * @param deciderType BUY if the user deciding is the buyer, or SELL if they are the seller
+   */
+  void declineMatch(Match match, Order.Type deciderType);
+}
---
+++ services/order/src/main/java/com/google/gfx/order/OrderService.java
@@ -0,0 +1,86 @@
+package com.google.gfx.order;
+
+import com.google.gfx.Order;
+import com.google.gfx.User;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
+
+/**
+ * Manages the orders in the system.
+ */
+public interface OrderService {
+
+  /**
+   * Stores an order.
+   */
+  void storeOrder(Order order);
+
+  /**
+   * Deletes an existing order.
+   */
+  void deleteOrder(Order order);
+
+  /**
+   * Closes an order after it is fulfilled.
+   *
+   * <p>The order should not be deleted as part of this operation.
+   */
+  void closeOrder(Order order);
+
+  /**
+   * Lists all existing orders.
+   */
+  // TODO(joaomartins): Implement pagination.
+  List<Order> listAllOrders();
+
+  // TODO(joaomartins): Implement pagination.
+  List<Order> listAllSellOrders();
+
+  // TODO(joaomartins): Implement pagination.
+  List<Order> listAllBuyOrders();
+
+  // TODO(joaomartins): Implement pagination.
+  List<Order> listSellOrdersForUser(User user);
+
+  // TODO(joaomartins): Implement pagination.
+  List<Order> listBuyOrdersForUser(User user);
+
+  /**
+   * Lists a number of buy orders by their amount.
+   *
+   * <p>This operation is useful to search similar orders to the one a user is just filing. For
+   * example, if a user wants to sell 20 USD, they will be presented with buy orders of value
+   * 20 USD, or the closest value, up or down, until {@code numOrders} results are obtained. If
+   * there are less than {@code numOrders} on the system, for the given {@code amount} and
+   * {@code currency}, all existing orders are listed.
+   *
+   * @param amount base amount around which to search
+   * @param currency currency being bought
+   * @param numOrders number of orders to be returned
+   * @return open {@code numOrders} buy orders with value closest to {@code amount} of
+   * {@code currency}
+   */
+  // TODO(joaomartins): Implement pagination.
+  List<Order> listBuyOrdersByAmount(BigDecimal amount, Currency currency, int numOrders);
+
+  /**
+   * Lists a number of sell orders by their amount.
+   *
+   *
+   * <p>This operation is useful to search similar orders to the one a user is just filing. For
+   * example, if a user wants to buy 20 USD, they will be presented with sell orders of value
+   * 20 USD, or the closest value, up or down, to a limit of {@code numOrders}. If there are less
+   * than {@code numOrders} on the system, for the given {@code amount} and {@code currency}, all
+   * existing orders are listed.
+   *
+   * @param amount base order amount to search for
+   * @param currency currency being sold
+   * @param numOrders number of orders to be returned
+   * @return open {@code numOrders} sell orders with value closest to {@code amount} of
+   * {@code currency}
+   */
+  // TODO(joaomartins): Implement pagination.
+  List<Order> listSellOrdersLowerByAmount(BigDecimal amount, Currency currency, int numOrders);
+}
